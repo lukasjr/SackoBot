786d7b1a2cbd9188408597db17da733cad72730b
Josh Lukas <joshuah.lukas@gmail.com>
2020-10-27 23:14:34 -0400

    Add total gif and accuracy to slash command

--- lambda/SackoBot/index.js
+++ lambda/SackoBot/index.js
@@ -43,13 +43,15 @@ function reply(event, envConfig, callback) {
   //if (!event.subtype && event.type === 'message' && (regex.test(messageText))) {
   console.log(`BOT NAME: ${event.bot_profile.name}`);
   console.log(`Sacko chance: ${envConfig.SACKO_CHANCE}`);
-  if (!event.subtype && event.bot_profile.name === 'giphy' && (isSacko(envConfig.SACKO_CHANCE))) {
+  if (!event.subtype && event.bot_profile.name === 'giphy') {
     // DynamoDB Put
+    const sackoFlag = isSacko(envConfig.SACKO_CHANCE);
     const params = {
       Item: {
         UserID: event.user,
         TimeStamp: event.ts,
         MessageText: messageText,
+        SackoFlag: sackoFlag
       },
       TableName: envConfig.DYNAMODB,
     };
@@ -61,54 +63,56 @@ function reply(event, envConfig, callback) {
       }
     });
 
-    const attachment = JSON.stringify([
-      {
-        fallback: 'The SACKO has been summoned',
-        color: '#2eb886',
-        title: `<@${event.user}> is the SACKO`,
-        title_link: 'https://66.media.tumblr.com/f5c030cba4abe8541661eb90f9237185/tumblr_inline_nvhjp2bCDU1qbdfh0_500.gif',
-        image_url: 'https://66.media.tumblr.com/f5c030cba4abe8541661eb90f9237185/tumblr_inline_nvhjp2bCDU1qbdfh0_500.gif',
-        thumb_url: 'http://example.com/path/to/thumb.png',
-        footer: 'SackoBot',
-        footer_icon: 'https://i.pinimg.com/originals/5b/99/d0/5b99d08f538428098192eddbd8b03e61.png',
-      },
-    ]);
+    if (sackoFlag) {
+      const attachment = JSON.stringify([
+        {
+          fallback: 'The SACKO has been summoned',
+          color: '#2eb886',
+          title: `<@${event.user}> is the SACKO`,
+          title_link: 'https://66.media.tumblr.com/f5c030cba4abe8541661eb90f9237185/tumblr_inline_nvhjp2bCDU1qbdfh0_500.gif',
+          image_url: 'https://66.media.tumblr.com/f5c030cba4abe8541661eb90f9237185/tumblr_inline_nvhjp2bCDU1qbdfh0_500.gif',
+          thumb_url: 'http://example.com/path/to/thumb.png',
+          footer: 'SackoBot',
+          footer_icon: 'https://i.pinimg.com/originals/5b/99/d0/5b99d08f538428098192eddbd8b03e61.png',
+        },
+      ]);
 
-    const message = JSON.stringify({
-      statusCode: 200,
-      token: envConfig.BOT_TOKEN,
-      channel: event.channel,
-      text: '',
-      attachments: attachment,
-    });
-
-    // HTTP POST request setup
-    const options = {
-      hostname: 'slack.com',
-      port: 443,
-      path: '/api/chat.postMessage',
-      method: 'POST',
-      headers: {
-        Authorization: `Bearer ${envConfig.BOT_TOKEN}`,
-        'Content-Type': 'application/json; charset=utf-8',
-        'Content-Length': message.length,
-      },
-    };
-
-    const req = https.request(options, (res) => {
-      // Slack response output
-      res.on('data', (d) => {
-        process.stdout.write(d);
+      const message = JSON.stringify({
+        statusCode: 200,
+        token: envConfig.BOT_TOKEN,
+        channel: event.channel,
+        text: '',
+        attachments: attachment,
       });
-    });
 
-    req.on('error', (error) => {
-      console.error(error);
-    });
+      // HTTP POST request setup
+      const options = {
+        hostname: 'slack.com',
+        port: 443,
+        path: '/api/chat.postMessage',
+        method: 'POST',
+        headers: {
+          Authorization: `Bearer ${envConfig.BOT_TOKEN}`,
+          'Content-Type': 'application/json; charset=utf-8',
+          'Content-Length': message.length,
+        },
+      };
 
-    // send HTTP POST with message as the data
-    req.write(message);
-    req.end();
+      const req = https.request(options, (res) => {
+        // Slack response output
+        res.on('data', (d) => {
+          process.stdout.write(d);
+        });
+      });
+
+      req.on('error', (error) => {
+        console.error(error);
+      });
+
+      // send HTTP POST with message as the data
+      req.write(message);
+      req.end();
+    }
   }
 }
 
@@ -116,14 +120,21 @@ function reply(event, envConfig, callback) {
 // count the number of sackos for each user
 function sackoCount(sackoData) {
   let totalSackos = {};
+  let totalGifs = {};
   var sackoCountText = '';
+
   for (let i = 0; i < sackoData.length; ++i) {
-    typeof totalSackos[sackoData[i].UserID] === 'undefined' ? totalSackos[sackoData[i].UserID] = 1 : totalSackos[sackoData[i].UserID]++;
+    typeof totalGifs[sackoData[i].UserID] === 'undefined' ? totalGifs[sackoData[i].UserID] = 1 : totalGifs[sackoData[i].UserID]++;
+    if (sackoData[i].SackoFlag) {
+      typeof totalSackos[sackoData[i].UserID] === 'undefined' ? totalSackos[sackoData[i].UserID] = 1 : totalSackos[sackoData[i].UserID]++;
+    }
   }
 
   for (let user in totalSackos) {
-    console.log(`<@${user}> has ${totalSackos[user]} sackos`);
-    sackoCountText += `<@${user}> has ${totalSackos[user]} sackos\n`;
+    let accuracy = totalSackos[user]/totalGifs[user]
+    let percentage = Math.round((accuracy + Number.EPSILON) * 100)
+    console.log(`<@${user}> has ${totalSackos[user]} sackos and ${totalGifs[user]} attempts - ${percentage}`);
+    sackoCountText += `<@${user}> has ${totalSackos[user]} sackos and ${totalGifs[user]} attempts - ${percentage}\n`;
   }
 
   sackoCountAttachment = {
